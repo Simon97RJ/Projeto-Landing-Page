ccaddc75c1d760d4c7cb8db130bce0758f3ddf4f
SimonRJ97 <pedrosimon.mendes97@gmail.com>
2023-11-24 19:40:59 -0300

    versao inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+var setaDireita = window.document.getElementById ("seta-direita")
+var leonardo = window.document.getElementById ("leonardo")
+var Samantha = window.document.getElementById ("samantha")
+var Bruna = window.document.getElementById ("Bruna")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+
+
+function RolarParaDireita (){
+    leonardo.style = "display:none"
+    Bruna.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = " display:flex; margin-top:55px" 
+
+}
+
+function RolarParaEsquerda(){
+    leonardo.style = "display:flex"
+    Bruna.style = "display:none"
+    setaDireita.style = "display:flex; margin-top:55px"
+    setaEsquerda.style = " display:none" 
+}
